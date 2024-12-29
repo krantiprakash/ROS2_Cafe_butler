c7fc5f5b92439151366e61f80c8d18d1e1362940
KRANTI PRAKASH <65568456+krantiprakash@users.noreply.github.com>
2024-12-29 15:50:57 +0530

    Update initial_pose

--- delivery_bot/scripts/task2.py
+++ delivery_bot/scripts/task2.py
@@ -200,8 +200,8 @@ class SequentialNav(Node):
         initial_pose.pose.pose.position.z = 0.0
         initial_pose.pose.pose.orientation.x = 0.0
         initial_pose.pose.pose.orientation.y = 0.0
-        initial_pose.pose.pose.orientation.z = 0.7071068
-        initial_pose.pose.pose.orientation.w = 0.7071068
+        initial_pose.pose.pose.orientation.z = 0.0
+        initial_pose.pose.pose.orientation.w = 1.0
         
         initial_pose.pose.covariance = [
             0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 
